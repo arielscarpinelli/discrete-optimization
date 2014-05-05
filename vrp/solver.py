d2e5dcf3f382bca7ccb883a8ac13c1f5a0c450d0
Ariel Scarpinelli <ascarpinelli@google.com>
2014-05-05 17:32:18 -0300

    fixed

--- vrp/solver.py
+++ vrp/solver.py
@@ -47,6 +47,8 @@ def segmentize(points):
     s.append(Segment(points[-1], points[0], l))
     return s    
 
+def length(customer1, customer2):
+    return math.sqrt((customer1.x - customer2.x)**2 + (customer1.y - customer2.y)**2)
 
 def solve_routing(customers, customerCount, vehicleCount, vehicleCapacity):
 
@@ -83,8 +85,6 @@ def solve_routing(customers, customerCount, vehicleCount, vehicleCapacity):
 
     assignment = routing.SolveWithParameters(parameters, None)
 
-    print assignment
-
     vehicle_tours = []
 
     for vehicle in range(vehicleCount):
@@ -108,6 +108,50 @@ def solve_routing(customers, customerCount, vehicleCount, vehicleCapacity):
     return (assignment.ObjectiveValue(), vehicle_tours)
 
 
+def solve_default(customers, customer_count, vehicle_count, vehicle_capacity):
+    #the depot is always the first customer in the input
+    depot = customers[0] 
+
+
+    # build a trivial solution
+    # assign customers to vehicles starting by the largest customer demands
+    vehicle_tours = []
+    
+    remaining_customers = set(customers)
+    remaining_customers.remove(depot)
+    
+    for v in range(0, vehicle_count):
+        # print "Start Vehicle: ",v
+        vehicle_tours.append([])
+        capacity_remaining = vehicle_capacity
+        while sum([capacity_remaining >= customer.demand for customer in remaining_customers]) > 0:
+            used = set()
+            order = sorted(remaining_customers, key=lambda customer: -customer.demand)
+            for customer in order:
+                if capacity_remaining >= customer.demand:
+                    capacity_remaining -= customer.demand
+                    vehicle_tours[v].append(customer)
+                    # print '   add', ci, capacity_remaining
+                    used.add(customer)
+            remaining_customers -= used
+
+    # checks that the number of customers served is correct
+    assert sum([len(v) for v in vehicle_tours]) == len(customers) - 1
+
+    # calculate the cost of the solution; for each vehicle the length of the route
+    obj = 0
+    for v in range(0, vehicle_count):
+        vehicle_tour = vehicle_tours[v]
+        if len(vehicle_tour) > 0:
+            obj += length(depot,vehicle_tour[0])
+            for i in range(0, len(vehicle_tour)-1):
+                obj += length(vehicle_tour[i],vehicle_tour[i+1])
+            obj += length(vehicle_tour[-1],depot)
+
+    vehicle_tours = [[customer.index for customer in tour] for tour in vehicle_tours]
+    return (obj, vehicle_tours)
+
+
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
@@ -125,14 +169,11 @@ def solve_it(input_data):
         parts = line.split()
         customers.append(Customer(i-1, int(parts[0]), float(parts[1]), float(parts[2])))
 
-    #the depot is always the first customer in the input
-    depot = customers[0] 
-
 
     # build a trivial solution
     # assign customers to vehicles starting by the largest customer demands
     
-    (obj, vehicle_tours) = solve_routing(customers, customer_count, vehicle_count, vehicle_capacity)
+    (obj, vehicle_tours) = solve_default(customers, customer_count, vehicle_count, vehicle_capacity)
 
     # prepare the solution in the specified output format
     outputData = str(obj) + ' ' + str(0) + '\n'
